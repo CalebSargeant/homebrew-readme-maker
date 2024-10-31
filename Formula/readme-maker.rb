125a6718d7c5fc7aded73a9a405a018c0715f562
Caleb Sargeant <4991715+CalebSargeant@users.noreply.github.com>
2024-10-31 17:16:36 +0100

    fix sha

--- Formula/readme-maker.rb
+++ Formula/readme-maker.rb
@@ -2,7 +2,7 @@ class ReadmeMaker < Formula
   desc "A CLI tool to generate structured README.md files using OpenAI API."
   homepage "https://github.com/CalebSargeant/readme-maker"
   url "https://github.com/CalebSargeant/readme-maker/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "74636617b9e30bc0bbbdb80db7e1c92e151ee3117aa62deb8f32b8d67a0e6140"
+  sha256 "383aba3b9c9c3fb19312f6d8d9eabc2c2ff629e0eef1ffd83ba454583aafd139"
   license "MIT"
 
   depends_on "python@3.9"  # Ensure Python 3.9 is installed
